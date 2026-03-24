eb431f64883fa2bd0a8f89feb5e5ce9815cb34d7
Iker Sevilla Sáez <240759033+ikersart@users.noreply.github.com>
2026-03-24 15:40:49 +0100

    Refactorizado script del menú

--- menu.sh
+++ menu.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+opcion_comprobar_servicios="Comprobar estado de los programas necesarios"
+opcion_comprobar_conexion="Comprobar conexión"
+opcion_ver_usuarios_conectados="Usuarios conectados"
+opcion_ver_puertos_abiertos="Ver puertos abiertos"
+
 while true
 do
 opcion=$(zenity --list \
 --title="Administrador del Sistema" \
 --column="Opciones" \
-"Comprobar estado de los programas necesarios" \
-"Comprobar conexión" \
-"Usuarios conectados" \
-"Ver puertos abiertos" \
-"Salir")
+"$opcion_comprobar_servicios" \
+"$opcion_comprobar_conexion" \
+"$opcion_ver_usuarios_conectados" \
+"$opcion_ver_puertos_abiertos")
 
 case $opcion in
-    "Comprobar estado de los programas necesarios")
-        ./comprobar_servicios.sh
+	"$opcion_comprobar_servicios")
+		./comprobar_servicios.sh
+	;;
+	"$opcion_comprobar_conexion")
+		IP=$(zenity --entry --text="Introduce la IP")
+		./comprobar_red.sh $IP
+	;;
+	"$opcion_ver_usuarios_conectados")
+		./usuarios.sh
+	;;
+	"$opcion_ver_puertos_abiertos")
+		./puertos.sh
         ;;
-    "Comprobar conexión")
-        IP=$(zenity --entry --text="Introduce la IP")
-        ./comprobar_red.sh $IP
-        ;;
-    "Usuarios conectados")
-        ./usuarios.sh
-        ;;
-    "Ver puertos abiertos")
-        ./puertos.sh
-        ;;
-    "Salir")
-        exit 0
-        ;;
+	"")
+		exit 0
+	;;
 esac
 
 done
