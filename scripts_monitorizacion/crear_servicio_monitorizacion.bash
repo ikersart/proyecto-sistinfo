1cde9cd7e5c475143ba524726840899553e7b1cc
Iker Sevilla Sáez <240759033+ikersart@users.noreply.github.com>
2026-03-27 12:12:37 +0100

    Reorganizadas las scripts de monitorización.

--- scripts_monitorizacion/crear_servicio_monitorizacion.bash
+++ scripts_monitorizacion/crear_servicio_monitorizacion.bash
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-	echo "Se requiere un solo parámetro: Ruta de la carpeta del proyecto (el repositorio)."
-	echo "Se recomienda lanzar desde dentro del repositorio con:"
-	echo "sudo $0 \$(pwd)"
-	exit 1
-fi
+directorio_de_esta_script="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ruta_repositorio=$1
-
-ruta_script_monitorizacion="$ruta_repositorio/monitorizacion.bash"
-
+ruta_script_servicio="$directorio_de_esta_script/monitorizacion.bash"
 ruta_configuracion_servicio="/etc/systemd/system/proyecto_sistinfo_monitorizacion.service"
 
 sudo echo "" >  "$ruta_configuracion_servicio"
@@ -29,7 +21,7 @@ sudo echo "User=root" >> "$ruta_configuracion_servicio"
 
 sudo echo "WorkingDirectory=$ruta_repositorio" >> "$ruta_configuracion_servicio"
 
-sudo echo "ExecStart=$ruta_script_monitorizacion" >> "$ruta_configuracion_servicio"
+sudo echo "ExecStart=$ruta_script_servicio" >> "$ruta_configuracion_servicio"
 
 sudo echo "Restart=always" >> "$ruta_configuracion_servicio"
 
