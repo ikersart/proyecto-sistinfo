696a6aa7dfeeb430131998f5eea4ad506d6ab054
Iker Sevilla Sáez <240759033+ikersart@users.noreply.github.com>
2026-03-27 08:52:49 +0100

    Eliminado chmod por motivos de seguridad y flexibilidad.

--- iniciar_servicio_monitorizacion.bash
+++ iniciar_servicio_monitorizacion.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo chmod 700 ./monitorizacion.bash
-
 sudo systemctl daemon-reload
 sudo systemctl enable proyecto_sistinfo_monitorizacion.service
 sudo systemctl restart proyecto_sistinfo_monitorizacion.service
