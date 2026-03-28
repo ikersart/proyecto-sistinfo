927e8c306125e4957b31b16c30325c077df8722f
Iker Sevilla Sáez <240759033+ikersart@users.noreply.github.com>
2026-03-28 07:46:11 +0100

    Ahora la configuración de systemd del servicio de monitorización viene de una plantilla configurable.

--- scripts_monitorizacion/crear_servicio_monitorizacion.bash
+++ scripts_monitorizacion/crear_servicio_monitorizacion.bash
@@ -1,34 +1,9 @@
 #!/bin/bash
 
 ruta_directorio_de_esta_script="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-ruta_script_servicio="$ruta_directorio_de_esta_script/monitorizacion.bash"
+ruta_plantilla_servicio="$ruta_directorio_de_esta_script/../configuracion/plantilla_monitorizacion.service.in"
 ruta_configuracion_servicio="/etc/systemd/system/proyecto_sistinfo_monitorizacion.service"
+export ruta_directorio_script_monitorizacion="$ruta_directorio_de_esta_script"
+export ruta_script_monitorizacion="$ruta_directorio_de_esta_script/monitorizacion.bash"
 
-sudo echo "" >  "$ruta_configuracion_servicio"
-
-sudo echo "[Unit]" >> "$ruta_configuracion_servicio"
-
-sudo echo "Description=Servicio de monitorización del servidor del proyecto de sistemas informáticos." >> "$ruta_configuracion_servicio"
-
-sudo echo "After=network.target" >> "$ruta_configuracion_servicio"
-
-sudo echo "" >> "$ruta_configuracion_servicio"
-
-sudo echo "[Service]" >> "$ruta_configuracion_servicio"
-
-sudo echo "User=root" >> "$ruta_configuracion_servicio"
-
-sudo echo "WorkingDirectory=$ruta_directorio_de_esta_script" >> "$ruta_configuracion_servicio"
-
-sudo echo "ExecStart=$ruta_script_servicio" >> "$ruta_configuracion_servicio"
-
-sudo echo "Restart=always" >> "$ruta_configuracion_servicio"
-
-sudo echo "" >> "$ruta_configuracion_servicio"
-
-sudo echo "[Install]" >> "$ruta_configuracion_servicio"
-
-sudo echo "WantedBy=multi-user.target" >> "$ruta_configuracion_servicio"
-
-sudo echo "" >> "$ruta_configuracion_servicio"
+envsubst < "$ruta_plantilla_servicio" > "$ruta_configuracion_servicio"
